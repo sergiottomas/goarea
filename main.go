8dd74d4340b577f591f7f3757d048491530f0536
= <=>
2019-12-13 00:26:03 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package goarena
+
+//Pi is a numeric proportion defined by relation between
+//the perimeter of a circumference and its perimeter
+const Pi = 3.1416
+
+//Circle is responsible to calculate the area of circuference
+func Circle(radius float64) float64 {
+	return radius * Pi
+}
+
+//Rect is responsible to calculate the area of the rectangle
+func Rect(base, height float64) float64 {
+	return base * height
+}
+
+func _TriangleEq(base, height float64) float64 {
+	return (base * height) / 2
+}
